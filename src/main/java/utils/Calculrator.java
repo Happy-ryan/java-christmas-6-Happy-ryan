68dbc77daa3ef97e9efdb952d89acef332bfda8e
happyryan <tjdgks120813@gmail.com>
2023-11-10 01:23:55 +0900

    feat(Calculator): add method to calculate total order price
    - Created the Calculator class to handle the calculation of the total order price based on the provided menu and quantity.
    - The method 'calculateTotalOrderPrice' computes the total price of the order by considering the quantity of each menu item and its corresponding price.

--- src/main/java/utils/Calculrator.java
+++ src/main/java/utils/Calculrator.java
@@ -1,2 +1,23 @@
-package utils;public class Calculrator {
+package utils;
+
+import menu.Menu;
+
+import java.text.DecimalFormat;
+import java.util.Map;
+
+public class Calculrator {
+
+    private final Map<String, Integer> order;
+
+    public Calculrator(Map<String, Integer> order) {
+        this.order = order;
+    }
+
+    public int calculateTotalOrderPrice() {
+        int totalOrderPrice = 0;
+        for (String menu : order.keySet()) {
+            totalOrderPrice += order.get(menu) * Menu.valueOf(menu).getPrice();
+        }
+        return totalOrderPrice;
+    }
 }
