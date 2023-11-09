6e1d0282bf14d00d0118305650fb06f24c2120a1
happyryan <tjdgks120813@gmail.com>
2023-11-10 00:38:01 +0900

    feat(OutputView): add OutputView class for printing various messages
    - Implemented the OutputView class to handle the printing of various messages, including order details, event information, benefits, total payment amounts, and event badges in the restaurant service.

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,65 @@
+package view;
+
+import java.util.Map;
+
+public class OutputView {
+    public static void printServiceStartMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public static void printDate(int date) {
+        System.out.println(date);
+    }
+
+    public static void printOrder(Map<String, Integer> order) {
+        StringBuilder sb = new StringBuilder();
+        for (String menu : order.keySet()) {
+            sb.append(menu).append("-").append(order.get(menu)).append(",");
+        }
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 1);
+        }
+        System.out.println(sb.toString());
+    }
+
+
+    public static void printEventPreviewMessage(int date) {
+        System.out.println("12월 " + date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
+    }
+
+    public static void printOderMenuMessage() {
+        System.out.println("<주문메뉴>");
+    }
+
+    public static void printOderMenu(Map<String, Integer> order){
+        for(String menu : order.keySet()){
+            System.out.println(menu + " " + order.get(menu) + "개");
+        }
+
+    }
+
+    public static void printTotalOrderPriceMessage() {
+        System.out.println("<할인 전 총주문 금액>");
+    }
+
+    public static void printBonusItemMessage() {
+        System.out.println("<증정 메뉴>");
+    }
+
+    public static void printBenefitsMessage() {
+        System.out.println("<혜택 내역>");
+    }
+
+    public static void printTotalBenefitAmountMessage() {
+        System.out.println("<총혜택 금액>");
+    }
+
+    public static void printExpectedPaymentAmountMessage() {
+        System.out.println("<할인 후 예상 결제 금액>");
+    }
+
+    public static void printEventBadgeMessage() {
+        System.out.println("<12월 이벤트 배지>");
+    }
+}
+
