83153f8da07f85c9a8aa75a994a99285d3aa9ce6
happyryan <tjdgks120813@gmail.com>
2023-11-10 14:34:19 +0900

    feat(Calculator): add methods for calculating weekday and weekend benefits
    - Added  method to calculate weekday benefit based on the number of dessert items ordered in 2023.
    - Added  method to calculate weekend benefit based on the number of main course items ordered in 2023.
    - Added  method to count the number of main course and dessert items in the order.

--- src/main/java/utils/Calculrator.java
+++ src/main/java/utils/Calculrator.java
@@ -1,13 +1,15 @@
 package utils;
 
 import menu.Menu;
+import menu.MenuCategory;
 
-import java.text.DecimalFormat;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Calculrator {
-
     private final Map<String, Integer> order;
+
+    Map<String, Integer> mainAndDessertCount = countMainAndDessert();
 
     public Calculrator(Map<String, Integer> order) {
         this.order = order;
@@ -20,4 +22,34 @@ public class Calculrator {
         }
         return totalOrderPrice;
     }
+
+    public int calculateWeekDayBenefit() {
+        int weekdaybenefit = 0;
+        weekdaybenefit += mainAndDessertCount.get("DESSERT") * 2023;
+        return weekdaybenefit;
+    }
+
+    public int calculateWeekeendBenefit() {
+        int weekendbenefit = 0;
+        weekendbenefit += mainAndDessertCount.get("MAIN") * 2023;
+        return weekendbenefit;
+    }
+
+    private Map<String, Integer> countMainAndDessert() {
+        mainAndDessertCount = new HashMap<>();
+        int MainMenuCount = 0;
+        int DessertMenutCount = 0;
+        for (String menu : order.keySet()) {
+            if (MenuCategory.MAIN.getMenuItems().contains(menu)) {
+                MainMenuCount++;
+            }
+            if (MenuCategory.DESSERT.getMenuItems().contains(menu)) {
+                DessertMenutCount++;
+            }
+        }
+        mainAndDessertCount.put("MAIN", MainMenuCount);
+        mainAndDessertCount.put("DESSERT", DessertMenutCount);
+        return mainAndDessertCount;
+    }
+
 }
