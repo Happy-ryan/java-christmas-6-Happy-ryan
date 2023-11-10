fb1866e33b73dc1a54e7069af2fd67306d26fcd5
happyryan <tjdgks120813@gmail.com>
2023-11-10 22:34:50 +0900

    style(MenuCategory): Rename BEVERAGE to DRINK

--- src/main/java/menu/MenuCategory.java
+++ src/main/java/menu/MenuCategory.java
@@ -7,7 +7,7 @@ public enum MenuCategory {
     APPETIZER(Arrays.asList("양송이수프", "타파스", "시저샐러드"), 0),
     MAIN(Arrays.asList("티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타"), 0),
     DESSERT(Arrays.asList("초코케이크", "아이스크림"), 0),
-    BEVERAGE(Arrays.asList("제로콜라", "레드와인", "샴페인"), 0);
+    DRINK(Arrays.asList("제로콜라", "레드와인", "샴페인"), 0);
 
     private final List<String> menuItems;
     private int count;
