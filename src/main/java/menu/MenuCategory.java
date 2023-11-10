88cc7460f37225bd232cbcb2a5ff89c824bc027e
happyryan <tjdgks120813@gmail.com>
2023-11-10 18:13:44 +0900

    refactor(menu): add  count field for each menu item category
    - Added a count field to the MenuCategory enum to keep track of the quantity of each menu item category. Also included methods to retrieve the menu items and increase the count.

--- src/main/java/menu/MenuCategory.java
+++ src/main/java/menu/MenuCategory.java
@@ -4,19 +4,29 @@ import java.util.List;
 import java.util.Arrays;
 
 public enum MenuCategory {
-    APPETIZER(Arrays.asList("양송이수프", "타파스", "시저샐러드")),
-    MAIN(Arrays.asList("티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타")),
-    DESSERT(Arrays.asList("초코케이크", "아이스크림")),
-    BEVERAGE(Arrays.asList("제로콜라", "레드와인", "샴페인"));
+    APPETIZER(Arrays.asList("양송이수프", "타파스", "시저샐러드"), 0),
+    MAIN(Arrays.asList("티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타"), 0),
+    DESSERT(Arrays.asList("초코케이크", "아이스크림"), 0),
+    BEVERAGE(Arrays.asList("제로콜라", "레드와인", "샴페인"), 0);
 
     private final List<String> menuItems;
+    private int count;
 
-    MenuCategory(List<String> menuItems) {
+    MenuCategory(List<String> menuItems, int count) {
         this.menuItems = menuItems;
+        this.count = count;
     }
 
     public List<String> getMenuItems() {
         return menuItems;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void increaseCount(int count) {
+        this.count += count;
     }
 }
 
