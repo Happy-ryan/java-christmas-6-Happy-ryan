091003d490e20b63da5001cc4c5fc9aa3f7951d8
happyryan <tjdgks120813@gmail.com>
2023-11-11 14:29:41 +0900

    feat(Application): implement the main program logic
    - Implemented the main logic of the program, including reading user input, calculating total order price, benefits, discounted payment, and printing the output.

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,50 @@
 package christmas;
 
+import utils.Calculrator;
+import view.InputView;
+import view.OutputView;
+
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView.printServiceStartMessage();
+
+        int date = InputView.readDate();
+        OutputView.printDate(date);
+
+        Map<String, Integer> order = InputView.readOrder();
+        OutputView.printOrder(order);
+
+        OutputView.printEventPreviewMessage(date);
+        System.out.println();
+
+        OutputView.printOderMenuMessage();
+        OutputView.printOderMenu(order);
+        System.out.println();
+
+        Calculrator calculrator = new Calculrator(date, order);
+        calculrator.calculateTotalOrderPrice();
+        Map<String, Integer> benefitByDate = calculrator.calculateBenefitByDate();
+
+        OutputView.printTotalOrderPriceMessage();
+        System.out.println();
+
+        OutputView.printBonusItemMessage();
+        System.out.println();
+
+        OutputView.printBenefitsMessage(benefitByDate);
+        System.out.println();
+
+        OutputView.printTotalBenefitAmountMessage();
+        System.out.println();
+
+        calculrator.calculateDiscountedPayment();
+
+        OutputView.printDiscountedPayment();
+        System.out.println();
+
+        OutputView.printEventBadgeMessage();
     }
 }
