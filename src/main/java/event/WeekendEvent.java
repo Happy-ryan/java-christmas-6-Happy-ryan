9fb05a5de94e9c5e85ddad8cce0a69a4f7fd952c
happyryan <tjdgks120813@gmail.com>
2023-11-13 21:27:42 +0900

    feat(event): add WeekendEvent class with benefit and discount calculations
    - Added the WeekendEvent class, extending the abstract Event class, with overridden methods for benefits and discount.

--- src/main/java/event/WeekendEvent.java
+++ src/main/java/event/WeekendEvent.java
@@ -1,2 +1,22 @@
-package event;public class WeekendEvent {
+package event;
+
+import menu.Receipt;
+
+public class WeekendEvent extends Event {
+    public WeekendEvent() {
+        super(EventDescription.WEEKEND);
+    }
+
+    @Override
+    public boolean tryUse(int day, Receipt receipt) {
+        return super.tryUse(day, receipt) && receipt.getMainCount() > 0;
+    }
+
+    public int getBenefit(int day, Receipt receipt) {
+        return getDescription().getBenefit() * receipt.getMainCount();
+    }
+
+    public int getDiscount(int day, Receipt receipt) {
+        return getDescription().getDiscount() * receipt.getMainCount();
+    }
 }
