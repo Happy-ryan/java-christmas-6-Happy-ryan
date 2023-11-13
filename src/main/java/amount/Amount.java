b7f1a786c90cf31a8c134332900cbcac68eef243
happyryan <tjdgks120813@gmail.com>
2023-11-13 23:44:34 +0900

    refactor(Amount): simplify Amount enum by removing the value field
    - Removed the value field from the Amount enum as it is not needed
    - Updated references in the code accordingly.

--- src/main/java/amount/Amount.java
+++ src/main/java/amount/Amount.java
@@ -1,35 +1,17 @@
 package amount;
 
 public enum Amount {
-    TOTAL_ORDER("<할인 전 총주문 금액>", 0),
-    TOTAL_BENEFIT("<총혜택 금액>", 0),
-    DISCOUNTED_PAYMENT("<할인 후 예상 결제 금액>", 0);
+    TOTAL_ORDER("<할인 전 총주문 금액>"),
+    TOTAL_BENEFIT("<총혜택 금액>"),
+    DISCOUNTED_PAYMENT("<할인 후 예상 결제 금액>");
 
     private final String description;
-    private int value;
 
-    Amount(String description, int initialValue) {
+    Amount(String description) {
         this.description = description;
-        this.value = initialValue;
     }
 
     public String getDescription() {
         return this.description;
-    }
-
-    public int getValue() {
-        return this.value;
-    }
-
-    public void increaseAmount(int amount) {
-        this.value += amount;
-    }
-
-    public void add(Amount other) {
-        this.value += other.getValue();
-    }
-
-    public void minus(Amount other) {
-        this.value -= other.getValue();
     }
 }
