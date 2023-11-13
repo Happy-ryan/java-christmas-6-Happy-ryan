46c013928e126150add30f8c3b2aedc9b2c8cd1f
happyryan <tjdgks120813@gmail.com>
2023-11-13 21:22:57 +0900

    feat(event): add SpecialEvent class with benefit and discount calculations
    - Added the SpecialEvent class, extending the abstract Event class, with overridden methods for benefits and discount

--- src/main/java/event/SpecialEvent.java
+++ src/main/java/event/SpecialEvent.java
@@ -1,2 +1,22 @@
-package event;public class SpecialEvent {
+package event;
+
+import menu.Receipt;
+
+public class SpecialEvent extends Event {
+    public SpecialEvent() {
+        super(EventDescription.SPECIAL);
+    }
+
+    @Override
+    public boolean tryUse(int day, Receipt receipt) {
+        return super.tryUse(day, receipt);
+    }
+
+    public int getBenefit(int day, Receipt receipt) {
+        return getDescription().getBenefit();
+    }
+
+    public int getDiscount(int day, Receipt receipt) {
+        return getDescription().getDiscount();
+    }
 }
