e0f463097f70f9fb329800f0c2dca994bbb481ec
happyryan <tjdgks120813@gmail.com>
2023-11-13 21:20:02 +0900

    feat(event): add GiftEvent class with benefit and discount calculations
    - Added the GiftEvent class, extending the abstract Event class, with overridden methods for benefits and discounts.

--- src/main/java/event/GiftEvent.java
+++ src/main/java/event/GiftEvent.java
@@ -1,2 +1,22 @@
-package event;public class GiftEvent {
+package event;
+
+import menu.Receipt;
+
+public class GiftEvent extends Event {
+    public GiftEvent() {
+        super(EventDescription.GIFT);
+    }
+
+    @Override
+    public boolean tryUse(int day, Receipt receipt) {
+        return super.tryUse(day, receipt) && receipt.getTotalPrice() >= 120000;
+    }
+
+    public int getBenefit(int day, Receipt receipt) {
+        return getDescription().getBenefit();
+    }
+
+    public int getDiscount(int day, Receipt receipt) {
+        return getDescription().getDiscount();
+    }
 }
