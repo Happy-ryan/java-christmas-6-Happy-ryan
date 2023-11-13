ff4ef229ab07107f9d99610878e3f1a33d12f7dd
happyryan <tjdgks120813@gmail.com>
2023-11-13 21:17:46 +0900

    feat(event): add ChristmasEvent class with benefit and discount calculations
    - Added the ChristmasEvent class, extending the abstract Event class, with overridden methods for benefits and discounts.

---
+++ src/main/java/event/ChristmasEvent.java
@@ -0,0 +1,21 @@
+package event;
+
+import menu.Receipt;
+
+public class ChristmasEvent extends Event{
+    public ChristmasEvent(){
+        super(EventDescription.CHRISTMAS);
+    }
+
+    @Override
+    public boolean tryUse(int day, Receipt receipt) {
+        return super.tryUse(day, receipt);
+    }
+
+    public int getBenefit(int day, Receipt receipt) {
+        return getDescription().getBenefit() + 100 * (day - 1);
+    }
+    public int getDiscount(int day, Receipt receipt) {
+        return getDescription().getDiscount() + 100 * (day - 1);
+    }
+}
