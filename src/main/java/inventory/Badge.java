473ba655d3fb7b477cdb5a417121d865b8735ecd
happyryan <tjdgks120813@gmail.com>
2023-11-15 14:47:37 +0900

    feat(inventory): add Badge enum for event badges
    - Introduce a Badge enum to manage information about event badges, including their names and benefit range.
    - Modify the OutputView class to utilize this enum in the printEventBadgeMessage method.

--- src/main/java/inventory/Badge.java
+++ src/main/java/inventory/Badge.java
@@ -1,2 +1,36 @@
-package inventory;public enum Badge {
+package inventory;
+
+public enum Badge {
+    UNKNOWN("없음", Integer.MIN_VALUE, Integer.MIN_VALUE),
+    STAR("별", 5000, 10000),
+    TREE("트리", 10000, 20000),
+    SANTA("산타", 20000, Integer.MAX_VALUE);
+
+    private final String name;
+    private final int minBenefit;
+    private final int maxBenefit;
+
+    Badge(String name, int minBenefit, int maxBenefit) {
+        this.name = name;
+        this.minBenefit = minBenefit;
+        this.maxBenefit = maxBenefit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isInRange(int benefit) {
+        return benefit >= minBenefit && benefit < maxBenefit;
+    }
+
+    public static Badge getBadgeForBenefit(int benefit) {
+        for (Badge badge : values()) {
+            if (badge.isInRange(benefit)) {
+                return badge;
+            }
+        }
+        return UNKNOWN;
+    }
 }
+
